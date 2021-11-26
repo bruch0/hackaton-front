e74f01cb71cac2fdea4b64d7d054d65553553502
bruch0 <lucas.bruch0@gmail.com>
2021-11-26 17:10:19 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyles from "./Shared/GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <div>CRA template</div>
+    </>
+  );
+};
+
+export default App;
