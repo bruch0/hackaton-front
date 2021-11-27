38b4ef96127c2632e0c047dac79be9f75b749f82
bruch0 <lucas.bruch0@gmail.com>
2021-11-27 13:05:36 -0300

    fix: eslint issues

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,14 @@
-import { Switch, Route } from "react-router-dom";
-export const Routes = () => {
-    return(
-        <Switch>
-            <Route path="/" exact>
-                <h1>Página home</h1>
-            </Route>
-        </Switch>
-    )
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <h1>Página home</h1>
+      </Route>
+    </Switch>
+  );
 }
+
+export default Routes;
