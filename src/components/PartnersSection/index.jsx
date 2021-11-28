28fd554742f38e5dafdb1603d315ea8de125d3b9
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2021-11-28 17:18:08 -0300

    fix: change partners-section description text and add alt text to companies logos

--- src/components/PartnersSection/index.jsx
+++ src/components/PartnersSection/index.jsx
@@ -6,16 +6,28 @@ function PartnersSection() {
   return (
     <Container>
       <h1>Empresas Parceiras</h1>
-      <p>Ajudamos pessoas a resolverem seus problemas</p>
+      <p>
+        Algumas de nossas principais parceiras para garantir os direitos de
+        LGBTQIA+
+      </p>
       <PartnersLogos>
-        <img src="./images/logo-leafs.png" alt="" />
-        <img src="./images/logo-circle.png" alt="" />
-        <img src="./images/logo-company1.png" alt="" />
-        <img src="./images/logo-raid-solucoes.png" alt="" />
-        <img src="./images/logo-virgula.png" alt="" />
-        <img src="./images/logo-compania-retangulos.png " alt="" />
-        <img src="./images/logo-leafs2.png" alt="" />
-        <img src="./images/logo-lorem-ipsum.png" alt="" />
+        <img src="./images/logo-leafs.png" alt="leafs company logo" />
+        <img src="./images/logo-circle.png" alt="circle company logo" />
+        <img src="./images/logo-company1.png" alt="company1 company logo" />
+        <img
+          src="./images/logo-raid-solucoes.png"
+          alt="raid solucoes company logo"
+        />
+        <img src="./images/logo-virgula.png" alt="virgula company logo" />
+        <img
+          src="./images/logo-compania-retangulos.png "
+          alt="compania retangulos company logo"
+        />
+        <img src="./images/logo-leafs2.png" alt="leafs 2 company logo" />
+        <img
+          src="./images/logo-lorem-ipsum.png"
+          alt="lorem-ipsum company logo"
+        />
         <img src="./images/logo" alt="" />
       </PartnersLogos>
     </Container>
