bb5821204f2bf4d3e7c35d62a27be6be155ff25d
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2021-11-28 15:34:35 -0300

    fix: remove react-smoothscroll-polyfill

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React from 'react';
-import smoothscroll from 'smoothscroll-polyfill';
 import Routes from './routes';
 import GlobalStyles from './Shared/GlobalStyles';
 
 function App() {
-  smoothscroll.polyfill();
-
   return (
     <>
       <GlobalStyles />
