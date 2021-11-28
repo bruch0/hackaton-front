de207490febc0441200883311f41b3c4a3d456da
bruch0 <lucas.bruch0@gmail.com>
2021-11-28 13:04:30 -0300

    feat: add initial login page

---
+++ src/pages/Login/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const LoginPage = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+// eslint-disable-next-line import/prefer-default-export
+export { LoginPage };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact>
         <Home />
+      </Route>
+      <Route path="/login" exact>
+        <Login />
       </Route>
     </Switch>
   );
